86233fd6913c65622c2c53ac4eb6c3bc6d93329f
Andrey Sakharov <a.sakharov@biletix.ru>
2020-06-21 00:30:16 +0300

    closing db connection on seeds adding error

--- src/seeds/index.ts
+++ src/seeds/index.ts
@@ -2,11 +2,14 @@ import { initDatabase } from '../init_db';
 import { Author, Book } from '../entities';
 import { authorSeeds } from './author.seed';
 import { bookSeeds } from './book.seed';
+import type { Connection } from 'typeorm';
 
 const addSeeds = async (): Promise<void> =>
 {
+   let conn: Connection | undefined;
+
    try {
-      const conn = await initDatabase();
+      conn = await initDatabase();
 
       await Author.save(
          authorSeeds.map(a => Author.create(a)),
@@ -19,6 +22,9 @@ const addSeeds = async (): Promise<void> =>
       console.info('the seeds were successfully inserted into the database...');
 
    } catch (err) {
+      if (conn !== undefined) {
+         await conn.close();
+      }
       console.error('seeds inserting error: ', err);
    }
 };
